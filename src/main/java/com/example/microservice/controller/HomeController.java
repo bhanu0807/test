f93653ad348b062fd8aeb2c3092c8c3a51f08481
bhanu0807 <140355168+bhanu0807@users.noreply.github.com>
2026-02-06 14:28:03 -0600

    fifth commit

--- src/main/java/com/example/microservice/controller/HomeController.java
+++ src/main/java/com/example/microservice/controller/HomeController.java
@@ -16,7 +16,7 @@ public class HomeController {
         this.messageService = messageService;
     }
 
-    /** Renders the home page. */
+    /** Renders the home page. ...*/
     @GetMapping("/")
     public String home() {
         return "home";
